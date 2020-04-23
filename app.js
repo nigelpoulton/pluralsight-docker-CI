8866fa2ee230e06a54db0f1fda7f04768bea44c5
Ethan Collins <ethancollins@ibm.com>
2020-04-23 12:22:58 -0500

    moves circleci yml file into .circleci folder

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
 // Simple node.js web app for demonstrating containerizing apps
 // For quick demo purposes only (not properly maintained)
-'use strict';
+"use strict";
 
-var express = require('express'),
-    app = express();
+var express = require("express"),
+	app = express();
 
-app.set('views', 'views');
-app.set('view engine', 'pug');
+app.set("views", "views");
+app.set("view engine", "pug");
 
-app.get('/', function(req, res) {
-    res.render('home', {
-  });
+app.get("/", function (req, res) {
+	res.render("home", {});
 });
 
 app.listen(8080);
